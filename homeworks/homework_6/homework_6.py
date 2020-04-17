94489ff0110646b235e917994c1a729e968eda7f
DepStudy <pystudy@mail.ru>
2020-04-17 15:29:30 +0300

    unfinished homework

---
+++ homeworks/homework_6/homework_6.py
@@ -0,0 +1,20 @@
+#Task 1:
+# import time
+# class TrafficLight:
+#     __color = None
+#
+#     def running(self):
+#         print('Сфетофор запущен')
+#         while True:
+#             self.__color = 'Red'
+#             print('Горит красный')
+#             time.sleep(7)
+#             self.__color = 'Yellow'
+#             print('Горит желтый')
+#             time.sleep(2)
+#             self.__color = 'Green'
+#             print('Горит зеленый')
+#             time.sleep(7)
+#
+# tl = TrafficLight()
+# tl.running()
